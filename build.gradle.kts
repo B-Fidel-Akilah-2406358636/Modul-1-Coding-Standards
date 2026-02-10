f12cc528ff15eeb8e74ef7ec8894e396a1054e91
Fidel <149506792+FidelAkilah@users.noreply.github.com>
2026-02-10 18:26:46 +0700

    Fixed unittest

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,13 +42,10 @@ dependencies {
     testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
     testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
     testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitJupiterVersion}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitJupiterVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter:${junitJupiterVersion}")
 }
 
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
-}
+
 
 tasks.register<Test>("unitTest") {
     description = "Runs unit tests."
@@ -66,4 +63,8 @@ tasks.register<Test>("functionalTest") {
     filter {
         includeTestsMatching("*FunctionalTest")
     }
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
