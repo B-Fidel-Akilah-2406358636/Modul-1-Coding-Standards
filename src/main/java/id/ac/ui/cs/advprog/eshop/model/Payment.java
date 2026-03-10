50baaa62067091807dae8efbb2fcdfaf31e59bac
Fidel <149506792+FidelAkilah@users.noreply.github.com>
2026-03-10 15:26:31 +0700

    [GREEN] Implement Payment model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,22 +1,49 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Getter
 public class Payment {
     private String id;
     private String method;
-    @Setter
     private String status;
     private Map<String, String> paymentData;
     private Order order;
 
     public Payment(Order order, String method, Map<String, String> paymentData) {
+        this.id = UUID.randomUUID().toString();
         this.order = order;
         this.method = method;
         this.paymentData = paymentData;
+        if ("Voucher Code".equals(method)) {
+            String voucherCode = paymentData.get("voucherCode");
+            if (voucherCode != null
+                    && voucherCode.length() == 16
+                    && voucherCode.startsWith("ESHOP")
+                    && voucherCode.replaceAll("\\D", "").length() == 8) {
+                this.status = "SUCCESS";
+            } else {
+                this.status = "REJECTED";
+            }
+        } else if ("Bank Transfer".equals(method)) {
+            String bankName = paymentData.get("bankName");
+            String referenceCode = paymentData.get("referenceCode");
+            if (bankName == null || bankName.isEmpty() || referenceCode == null || referenceCode.isEmpty()) {
+                this.status = "REJECTED";
+            } else {
+                this.status = "SUCCESS";
+            }
+        }
+    }
+
+    public void setStatus(String status) {
+        if ("SUCCESS".equals(status) || "REJECTED".equals(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
